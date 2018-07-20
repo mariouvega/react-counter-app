e2ede9de84e70b167d0420ef1f2ea829d68d5464
mariouvega <mario.vega014@gmail.com>
2018-07-20 00:08:08 -0400

    added a function and state to a component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,9 +3,25 @@ import React, { Component } from 'react';
 // right her we can eliminate `export default Counter line` w/ (following line) 
 // export default class Counter extends Component
 class Counter extends Component {
+	// state is an object that stores any data this component needs
+	state = {
+		count: 0,
+	};
+
 	render() {
-		return <h1>Hello World!</h1>
+		return (
+			<React.Fragment>
+				<span>{this.formatCount()}</span>
+				<button>Increment</button>
+			</React.Fragment>
+		);
+	}
+	formatCount() {
+		const count = this.state.count;
+		const x = <h1>ZERO</h1>;
+		return count === 0 ? x : count;
 	}
 }
+
 
 export default Counter;
