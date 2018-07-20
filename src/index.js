19b6936f9ba35fdc569ce655fcf620e7f983c329
mariouvega <mario.vega014@gmail.com>
2018-07-19 23:10:23 -0400

    added bootstrap 4.1.1

--- src/index.js
+++ src/index.js
@@ -7,6 +7,5 @@ import registerServiceWorker from './registerServiceWorker';
 // here we are importing bootstrap into our app
 import 'bootstrap/dist/css/bootstrap.css';
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
